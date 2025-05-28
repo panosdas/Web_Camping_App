105ebad2ae58d8df394c6cc42fc1b4dea2d9d9a1
Panos Dasargyris <panosdasargyris@gmail.com>
2025-05-28 14:47:15 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ app.use("/",router);
 
 const port = process.env.PORT || '3000';
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
